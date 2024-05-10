c14af65cae5fc09020fd3b485664c5bed4ab2696
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-05-10 15:16:56 +0900

    Update LibMain.cs
    commit test

--- Lib/LibMain.cs
+++ Lib/LibMain.cs
@@ -9,12 +9,7 @@ namespace Lib
     {
         public void Run()
         {
-            TcpClient tc = new TcpClient("127.0.0.1", 12345);
-            StreamCommunicateProcess pro = new(tc.GetStream());
-            //pro.WriteAsync(new byte[] {55,56,57,58,59,60,61 });
-            //pro.WriteAsync("345hgy54");
-
-            pro.F();
+            
         }
     }
 }
