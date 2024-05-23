05051052e687fed58a5b719715e324336cd38960
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-05-23 09:08:46 +0900

    new process run

---
+++ Lib/CS/ProcessProcess.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace Lib.CS
+{
+    internal class ProcessProcess
+    {
+        public void Run()
+        {
+            const string FOLDER = @"D:\Folder";
+            const string FILE = "ExeFile.exe";
+
+            run(FOLDER, FILE);
+        }
+
+        public void run(string folder, string file)
+        {
+            ProcessStartInfo psi = new()
+            {
+                FileName = Path.Combine(folder, file),
+                WorkingDirectory = folder, //new process set path
+            };
+            Process.Start(psi);
+        }
+    }
+}
