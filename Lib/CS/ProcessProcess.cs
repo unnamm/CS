12fcbcd47fc6f6e55a92877b80135fdafc7de845
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-05-24 11:33:20 +0900

    process async

--- Lib/CS/ProcessProcess.cs
+++ Lib/CS/ProcessProcess.cs
@@ -9,17 +9,36 @@ namespace Lib.CS
             const string FOLDER = @"D:\Folder";
             const string FILE = "ExeFile.exe";
 
-            run(FOLDER, FILE);
+            //run(FOLDER, FILE);
+            runAsync(FOLDER, FILE, 5000);
         }
 
-        public void run(string folder, string file)
+        private void run(string folder, string file)
         {
             ProcessStartInfo psi = new()
             {
                 FileName = Path.Combine(folder, file),
                 WorkingDirectory = folder, //new process set path
             };
-            Process.Start(psi);
+            var pro = Process.Start(psi);
+            pro.WaitForInputIdle(); //wait process open
+        }
+
+        private async void runAsync(string folder, string file, int timeout)
+        {
+            var task = Task.Run(() =>
+            {
+                run(folder, file);
+            });
+
+            var end = await Task.WhenAny(task, Task.Delay(timeout));
+
+            if (end != task)
+            {
+                throw new TimeoutException();
+            }
+
+            //run after open
         }
     }
 }
