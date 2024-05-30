6c1e32ace957271020f38bab07321b14cd4ce034
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-05-30 18:29:47 +0900

    add content into txt file

---
+++ Lib/CS/FileTxtProcess.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace Lib.CS
+{
+    internal class FileTxtProcess
+    {
+        public void AddTextAndCreate(string file, string add)
+        {
+            if (File.Exists(file) == false)
+            {
+                var v = File.Create(file);
+                v.Dispose(); //file.create is filestream. need dispose
+            }
+
+            var read = File.ReadAllText(file);
+            File.WriteAllText(file, read + add); //auto enter line
+        }
+    }
+}
