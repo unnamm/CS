7f3fc6b97158631846e6171691e6a022e5fc1b6c
이재승 <156874341+Leejaeseung1@users.noreply.github.com>
2024-06-10 10:15:10 +0900

    array select2

--- Lib/CS/ArrayProcess.cs
+++ Lib/CS/ArrayProcess.cs
@@ -11,6 +11,23 @@ namespace Lib.CS
             IEnumerable<int> v2 = arr.Select((x, i) => x + i); //{100, 201, 302}, i: 0,1,2,...
         }
 
+        private class Temp
+        {
+            public int F()
+            {
+                int data = 1;
+                Console.WriteLine(data);
+                return data;
+            }
+        }
+
+        public void Select2()
+        {
+            Temp[] values = new[] { new Temp(), new Temp() };
+            var v = values.Select(x => x.F()); //{1, 1}; //no run Console.WriteLine
+            values.Select(x => x.F()).ToArray(); // {1, 1}; // run Console.WriteLine
+        }
+
         public void Join()
         {
             var arr = new int[] { 100, 200, 300 };
