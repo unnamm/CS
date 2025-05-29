a72b068579fc2b7da0190e0404f9e6b3086b8777
이재승 <ljs01@jdtek.co.kr>
2025-05-29 17:12:52 +0900

    update tuple

--- Lib/CS/TupleProcess.cs
+++ Lib/CS/TupleProcess.cs
@@ -6,22 +6,35 @@ namespace Lib.CS
         public void F()
         {
             var v = f();
-            var item1 = v.Item1;
-            var item2 = v.Item2;
+            var item11 = v.Item1;
+            var item12 = v.Item2;
 
             var v2 = f2();
-            var item3 = v2.Item1;
-            var item4 = v2.Item2;
+            var item21 = v2.Item1;
+            var item22 = v2.Item2;
+
+            var v3 = f3();
+            var item31 = v3.A;
+            var item32 = v3.B;
+
+            var (A, B) = f3();
+            var item33 = A;
+            var item34 = B;
         }
 
         private static Tuple<int, string> f()
         {
-            return Tuple.Create(1, "a");
+            return Tuple.Create(1, "one");
         }
 
         private static (int, string) f2()
         {
-            return (5, "aa");
+            return (2, "two");
+        }
+
+        private static (int A, string B) f3()
+        {
+            return (3, "three");
         }
     }
 }
