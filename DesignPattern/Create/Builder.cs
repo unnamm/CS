aec5725b8b6f7bc1d6fa0d933e118fe7007c3856
이재승 <ljs01@jdtek.co.kr>
2025-08-04 18:23:43 +0900

    add build pattern

--- DesignPattern/Create/Builder.cs
+++ DesignPattern/Create/Builder.cs
@@ -8,9 +8,52 @@ namespace DesignPattern.Create
 {
     public class Builder
     {
+        class Wrong1 // so many child class
+        {
+            interface House { }
+            class GardenHouse : House { }
+            class SwimHouse : House { }
+        }
+
+        class wrong2 //so many parameters
+        {
+            class House
+            {
+                public House(bool isGarden, bool isSwim) { }
+            }
+        }
+
+        class House { }
+
+        class HouseBuilder
+        {
+            private House _current = new();
+
+            public HouseBuilder BuildWall() { return this; }
+            public HouseBuilder BuildDoor(int doorCount) { return this; }
+            public HouseBuilder BuildWindow() { return this; }
+            public HouseBuilder BuildRoof(bool isColor) { return this; }
+            public HouseBuilder BuildGarden() { return this; }
+            public HouseBuilder BuildSwim(int swimSize) { return this; }
+            public House GetHouse() => _current;
+        }
+
         public static void Sample()
         {
+            HouseBuilder builder = new();
+            House swimHouse = builder
+                .BuildWall()
+                .BuildWindow()
+                .BuildDoor(3)
+                .BuildSwim(22)
+                .GetHouse();
 
+            builder = new();
+            House gardenHouse = builder
+                .BuildWall()
+                .BuildRoof(true)
+                .BuildGarden()
+                .GetHouse();
         }
     }
 }
--- Run/Program.cs
+++ Run/Program.cs
@@ -2,8 +2,6 @@
 
 Console.WriteLine("Hello, World!");
 
-var mordern = DesignPattern.Create.AbstractFactory.Sample("Mordern");
-var chair = mordern.Item1;
-var sofa = mordern.Item2;
+DesignPattern.Create.Builder.Sample();
 
 Console.ReadLine();
