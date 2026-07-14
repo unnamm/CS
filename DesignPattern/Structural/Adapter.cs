a73dee56bb678a316296091dbde31647f8b44ed3
이재승 <ljs01@jdtek.co.kr>
2026-07-14 17:12:55 +0900

    adapter

---
+++ DesignPattern/Structural/Adapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Structural
+{
+    public class Adapter
+    {
+        interface IBehaviour
+        {
+            void Func();
+        }
+
+        class BehaviourAdapter : IBehaviour
+        {
+            private readonly Legacy _legacy;
+            public BehaviourAdapter(Legacy legacy) => _legacy = legacy;
+            public void Func() => _legacy.Go();
+        }
+
+        class Legacy
+        {
+            public void Go() { }
+        }
+    }
+}
