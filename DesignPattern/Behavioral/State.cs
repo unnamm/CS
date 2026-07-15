900ce22ca0261d962ac2a905658ed6ff8253db98
이재승 <ljs01@jdtek.co.kr>
2026-07-15 17:39:54 +0900

    state

---
+++ DesignPattern/Behavioral/State.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Behavioral
+{
+    public class State
+    {
+        interface ILight
+        {
+            ILight On();
+        }
+
+        class Green : ILight
+        {
+            public ILight On()
+            {
+                Console.WriteLine("green");
+                return new Yellow();
+            }
+        }
+
+        class Yellow : ILight
+        {
+            public ILight On()
+            {
+                Console.WriteLine("yellow");
+                return new Red();
+            }
+        }
+
+        class Red : ILight
+        {
+            public ILight On()
+            {
+                Console.WriteLine("red");
+                return new Green();
+            }
+        }
+
+        class Blinker
+        {
+            private ILight _current;
+
+            public Blinker(ILight currnet) => _current = currnet;
+
+            public ILight Blink()
+            {
+                _current = _current.On();
+                return _current;
+            }
+            public ILight CurrentColor() => _current;
+        }
+
+        public static void Sample()
+        {
+            Blinker blinker = new(new Green());
+            blinker.Blink();
+            blinker.Blink();
+            blinker.Blink();
+            var currentColor = blinker.CurrentColor();
+        }
+    }
+}
