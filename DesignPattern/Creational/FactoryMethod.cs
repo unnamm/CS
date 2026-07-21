13b570ff1c9100a0221979ba13c3ec751bb0c32a
이재승 <ljs01@jdtek.co.kr>
2026-07-21 18:02:19 +0900

    update factorymethod

---
+++ DesignPattern/Creational/FactoryMethod.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Creational
+{
+    public class FactoryMethod
+    {
+        abstract class Factory
+        {
+            protected abstract object Create();
+
+            public string CreateString() => Create().ToString()!;
+        }
+
+        class StringFactory : Factory
+        {
+            protected override object Create() => "string";
+        }
+        class IntegerFactory : Factory
+        {
+            protected override object Create() => 1;
+        }
+
+        public static void Sample()
+        {
+            Factory f = new StringFactory();
+            var str1 = f.CreateString();
+            f = new IntegerFactory();
+            var str2 = f.CreateString();
+        }
+    }
+}
