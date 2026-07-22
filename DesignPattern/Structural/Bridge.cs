249a531376bbe198143d7ad8b8a2bb9fccb22fe7
이재승 <ljs01@jdtek.co.kr>
2026-07-22 10:20:54 +0900

    update flyweight

---
+++ DesignPattern/Structural/Bridge.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Structural
+{
+    public class Bridge
+    {
+        interface IRender
+        {
+            void Circle(double radius);
+            void Square(double line);
+        }
+        class Vector : IRender
+        {
+            public void Circle(double radius) => Console.WriteLine("draw vector");
+            public void Square(double line) => Console.WriteLine("draw vector");
+        }
+        class Pixel : IRender
+        {
+            public void Circle(double radius) => Console.WriteLine("draw pixel");
+            public void Square(double line) => Console.WriteLine("draw pixel");
+        }
+
+        abstract class Shape
+        {
+            protected IRender _render;
+            protected Shape(IRender render) => _render = render;
+            public abstract void Draw();
+        }
+        class Circle : Shape
+        {
+            private double _radius;
+            public Circle(IRender render, double radius) : base(render) => _radius = radius;
+            public override void Draw() => _render.Circle(_radius);
+        }
+        class Square : Shape
+        {
+            private double _line;
+            public Square(IRender render, double radius) : base(render) => _line = radius;
+            public override void Draw() => _render.Square(_line);
+        }
+
+        public static void Sample()
+        {
+            var circle = new Circle(new Pixel(), 5);
+            circle.Draw();
+
+            var square = new Square(new Vector(), 3);
+            square.Draw();
+        }
+    }
+}
---
+++ DesignPattern/Structural/Flyweight.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Structural
+{
+    public class Flyweight
+    {
+        class CharacterStyle
+        {
+            public string? FontFamily;
+            public int FontSize;
+        }
+
+        class StyleFactory
+        {
+            private static readonly Dictionary<string, CharacterStyle> _cache = [];
+
+            public static CharacterStyle GetStyle(string font, int size)
+            {
+                var key = $"{font}_{size}";
+                if (!_cache.ContainsKey(key))
+                {
+                    _cache[key] = new CharacterStyle { FontFamily = font, FontSize = size };
+                }
+                return _cache[key];
+            }
+        }
+
+        class Character
+        {
+            public char Symbol;
+            public CharacterStyle? Style;
+        }
+
+        public static void Sample()
+        {
+            var style = StyleFactory.GetStyle("Arial", 12);
+            var c1 = new Character { Symbol = 'A', Style = style };
+            var c2 = new Character { Symbol = 'B', Style = style };
+
+            style = StyleFactory.GetStyle("Consolas", 10);
+            var c3 = new Character { Symbol = 'C', Style = style };
+        }
+    }
+}
