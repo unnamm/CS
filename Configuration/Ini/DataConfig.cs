d4e6a7745fcb5a75adc9f3c41455a08663c9e33d
이재승 <ljs01@jdtek.co.kr>
2026-07-23 11:16:10 +0900

    upgrade net10.0

---
+++ Configuration/Ini/DataConfig.cs
@@ -0,0 +1,34 @@
+﻿namespace Configuration.Ini
+{
+    public class DataConfig : ConfigBase
+    {
+        public int ValueInt;
+        public bool ValueBool;
+        public string ValueString = string.Empty;
+        public string[] ValueArray = [];
+
+        public DataConfig(string filePath) : base(filePath)
+        {
+            base.Get(ref ValueInt, "data1");
+            base.Get(ref ValueBool, "data1");
+            base.Get(ref ValueString, "data1");
+
+            base.Get(ref ValueInt, "data2");
+            base.Get(ref ValueBool, "data2");
+            base.Get(ref ValueString, "data2");
+
+            base.Get(ref ValueArray, "data3");
+        }
+
+        public void Save()
+        {
+            base.Set(ValueInt, "data1");
+            base.Set(ValueBool, "data1");
+            base.Set(ValueString, "data1");
+
+            base.Set(ValueInt, "data2");
+            base.Set(ValueBool, "data2");
+            base.Set(ValueString, "data2");
+        }
+    }
+}
--- Run/Program.cs
+++ Run/Program.cs
@@ -10,15 +10,8 @@ async Task Run()
 {
     try
     {
-        Database.Database db = new Database.SQLite(@"local path .db");
-        //db = new Database.MSSQL("127.0.0.1", "user", "password");
-        await db.ConnectAsync(default);
+        var c = new Configuration.Ini.DataConfig(@"Ini\DataConfig.ini");
 
-        var rows = await db.ReaderAsync("SELECT * FROM TableNmae");
-        var countresult = await db.ScalarAsync("SELECT COUNT(*) FROM TableName");
-
-        var now = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
-        var affect = await db.NonQueryAsync($"INSERT INTO TableName (DateTime, Description, Result) VALUES ('{now}', 'temp message', 1)");
     }
     catch (Exception ex)
     {
