d9b905d306ccdf7bfca84bfa8e7557bb0b291c23
이재승 <ljs01@jdtek.co.kr>
2026-07-24 10:00:03 +0900

    json move config

---
+++ Configuration/Json/DataConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Configuration.Json
+{
+    public class DataConfig : JsonBase
+    {
+        public DataConfig(string filePath) : base(filePath) { }
+
+        [JsonPropertyName("integer")]
+        public int DataInt { get; set; }
+
+        [JsonPropertyName("string")]
+        public string? DataString { get; set; }
+
+        [JsonPropertyName("double")]
+        public double DataDouble { get; set; }
+
+        [JsonPropertyName("bool")]
+        public bool DataBool { get; set; }
+
+        [JsonPropertyName("array")]
+        public int[]? Array { get; set; }
+
+        [JsonPropertyName("pair")]
+        public Dictionary<int, string>? Dic { get; set; }
+    }
+}
---
+++ Configuration/Json/JsonBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Configuration.Json
+{
+    public abstract class JsonBase
+    {
+        private readonly string _filePath;
+
+        protected JsonBase(string filePath)
+        {
+            _filePath = filePath;
+            Load();
+        }
+
+        public void Load()
+        {
+            if (!File.Exists(_filePath))
+            {
+                Save();
+                return;
+            }
+        }
+
+        public void Save()
+        {
+            
+        }
+    }
+}
